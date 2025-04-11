12786d60fe4a9983fd0529beab1c69a8e532ae8e
IgnacioBritoss <167232030+IgnacioBritoss@users.noreply.github.com>
2025-04-11 08:50:42 -0300

    LISTO

---
+++ Assets/Scripts/gustos.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gustos : MonoBehaviour
+
+{
+    public string codigoHelado;
+    public float gramosHelado;
+    float precioGramo = 1.250f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (codigoHelado == "" || gramosHelado > 3000 || gramosHelado < 250)
+        {
+            Debug.Log("los valores son invalidos");
+            return;
+        }
+
+        
+        if(codigoHelado == "FRU")
+        {
+            Debug.Log("el precio final es  "+ gramosHelado * precioGramo * 0.9f + "$"); 
+        }
+        else if ("" + codigoHelado == "CHO")
+        {
+            Debug.Log("el precio final es  " + gramosHelado * precioGramo + "$");
+
+        } else if(codigoHelado == "DDL")
+        {
+            Debug.Log("el precio final es  " + gramosHelado * precioGramo + "$");
+        }
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
